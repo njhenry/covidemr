2694851787be77113cb1cc09ab41aac7a40a0eca
Nat <nat.henry7@gmail.com>
2020-08-18 21:14:21 -0700

    Stub to prepare Italy data.

---
+++ exec/02_ita_data_prep.R
@@ -0,0 +1,14 @@
+## -----------------------------------------------------------------------------
+## 
+## Data Preparation Code for Italy ISTAT deaths, population, and covariates
+## 
+## For more details, see README at https://github.com/njhenry/covidemr/
+## 
+## -----------------------------------------------------------------------------
+
+## DEVELOPMENT: rebuild library
+devtools::load_all("/homes/nathenry")
+
+library(data.table)
+library(covidemr)
+
