d81d1757028c407543aff23474efe337818e857c
Nat Henry <nat.henry7@gmail.com>
2020-12-11 01:25:04 -0800

    Use conda to install all necessary packages.

--- conda_create_R_environment.sh
+++ conda_create_R_environment.sh
@@ -1,3 +1,13 @@
+##
+## Execute this script with a single argument to specify the environment name
+## Default environment is "r_env"
+## Example: ./conda_create_R_environment.sh my_new_env
+##
+ENV_NAME=${1:-r_env}
+
 conda activate base &&
-  conda create -n r_env -y -c conda-forge r-base=4.0.3 r-essentials r-renv r-devtools &&
-  conda activate r_env;
+  conda create -n $ENV_NAME -y -c conda-forge r-base=4 gxx_linux-64 gdal r-sf \
+    r-devtools r-renv &&
+  conda activate $ENV_NAME &&
+  pip install -U radian &&
+  R --vanilla -e "devtools::install_github('jalvesaq/colorout')";
