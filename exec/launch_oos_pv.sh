48bb85e2fe80f3669b9dc90c613181773ed581c6
Nat Henry <nat.henry7@gmail.com>
2021-04-30 14:48:18 -0700

    Update launch commands with new run dates.

--- exec/launch_oos_pv.sh
+++ exec/launch_oos_pv.sh
@@ -1,30 +1,3 @@
-qsub -N f3fal_m_is ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210113f3fal && sleep 0.25 &&
-qsub -N f2fal_m_is ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210113f2fal && sleep 0.25 &&
-qsub -N f1fal_m_is ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210113f1fal && sleep 0.25 &&
-qsub -N f3fal_f_is ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex female --data-version 20210113 --model-version 20210113f3fal && sleep 0.25 &&
-qsub -N f2fal_f_is ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex female --data-version 20210113 --model-version 20210113f2fal && sleep 0.25 &&
-qsub -N f1fal_f_is ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex female --data-version 20210113 --model-version 20210113f1fal && sleep 0.25 &&
-qsub -N f3fal_m_oos ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210113f3fal --oos && sleep 0.25 &&
-qsub -N f2fal_m_oos ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210113f2fal --oos && sleep 0.25 &&
-qsub -N f1fal_m_oos ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210113f1fal --oos && sleep 0.25 &&
-qsub -N f3fal_f_oos ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex female --data-version 20210113 --model-version 20210113f3fal --oos && sleep 0.25 &&
-qsub -N f2fal_f_oos ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex female --data-version 20210113 --model-version 20210113f2fal --oos && sleep 0.25 &&
-qsub -N f1fal_f_oos ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex female --data-version 20210113 --model-version 20210113f1fal --oos && sleep 0.25 &&
-qsub -N f3fl_m_is ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210113f3fl && sleep 0.25 &&
-qsub -N f2fl_m_is ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210113f2fl && sleep 0.25 &&
-qsub -N f1fl_m_is ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210113f1fl && sleep 0.25 &&
-qsub -N f3fl_f_is ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex female --data-version 20210113 --model-version 20210113f3fl && sleep 0.25 &&
-qsub -N f2fl_f_is ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex female --data-version 20210113 --model-version 20210113f2fl && sleep 0.25 &&
-qsub -N f1fl_f_is ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex female --data-version 20210113 --model-version 20210113f1fl && sleep 0.25 &&
-qsub -N f3fl_m_oos ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210113f3fl --oos && sleep 0.25 &&
-qsub -N f2fl_m_oos ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210113f2fl --oos && sleep 0.25 &&
-qsub -N f1fl_m_oos ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210113f1fl --oos && sleep 0.25 &&
-qsub -N f3fl_f_oos ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex female --data-version 20210113 --model-version 20210113f3fl --oos && sleep 0.25 &&
-qsub -N f2fl_f_oos ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex female --data-version 20210113 --model-version 20210113f2fl --oos && sleep 0.25 &&
-qsub -N f1fl_f_oos ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex female --data-version 20210113 --model-version 20210113f1fl --oos && sleep 0.25 &&
-qsub -N nof_m_is ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210113nof && sleep 0.25 &&
-qsub -N nof_f_is ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex female --data-version 20210113 --model-version 20210113nof && sleep 0.25 &&
-qsub -N nof_m_oos ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210113nof --oos && sleep 0.25 &&
-qsub -N nof_f_oos ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex female --data-version 20210113 --model-version 20210113nof --oos && sleep 0.25 &&echo "================================================================" &&
+qsub -N m_f2fal ~/repos/covidemr/exec/cluster_R_exec.sh ~/repos/covidemr/exec/04b_predictive_validity_metrics.R --run-sex male --data-version 20210113 --model-version 20210421_f2fal && sleep 0.25 &&
 echo "============================= DONE =============================" &&
 echo "================================================================";
